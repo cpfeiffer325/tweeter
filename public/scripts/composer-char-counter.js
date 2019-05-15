96e7df7823614dea1bf3f94759d83a00fd3c2549
Christian <c_pfeiffer325@yahoo.ca>
2019-05-15 01:57:14 +0000

    Refactor character counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,13 +1,7 @@
 $(document).ready(function() {
-    console.log("char counter is still running!")
-
     $("textarea").on("input", function() {
-        let char = 0;
-        char = $("textarea").val().length;
-        if (char > 140) {
-            $("span.counter").text(140 - char).css({color: "red"});
-        } else {
-            $("span.counter").text(140 - char).css({color: "black"});
-		}
-      });
-  });
+        let textLength = $("textarea").val().length;
+        let color = (textLength <= 140) ? "black" : "red";
+        $("#counter").text(140 - textLength).css({color: color});
+    });
+});
