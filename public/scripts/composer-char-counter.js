219c5a914136ccdf14b60ff7ec4bec74181518d8
cpfeiffer325 <cpfeiffer325@gmail.com>
2019-05-18 14:10:18 -0600

    add comments to character counter file

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
     $("textarea").on("input", function() {
+        // checks length of the text input
         let textLength = $("textarea").val().length;
+        // changes counter to red if exceeding 140 characters
         let color = (textLength <= 140) ? "black" : "red";
+        // updates text counter on the app
         $("#counter").text(140 - textLength).css({color: color});
     });
 });
